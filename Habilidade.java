9d55cafe742f92f5df174e55413c8060d87b30b8
eduardoalm <eduardo.hevangelista@gmail.com>
2025-11-26 14:53:18 -0300

    Criação da classe Habilidade com a função poderEfeito(returna o efeito da habilidade)

--- Habilidade.java
+++ Habilidade.java
@@ -1,3 +1,25 @@
 public class Habilidade {
-    
+    String nome;
+    int multiplicador;
+
+    public Habilidade(String nome, int multiplicador) {
+        this.nome = nome;
+        this.multiplicador = multiplicador;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getMultiplicador() {
+        return multiplicador;
+    }
+    public void setMultiplicador(int multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+
+    public int poderEfeito() {
+        return getMultiplicador();
+    }
 }
