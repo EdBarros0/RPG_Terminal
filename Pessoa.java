e7200056866f09d99e02d3051704633e4f13c6d8
eduardoalm <eduardo.hevangelista@gmail.com>
2025-11-26 14:15:29 -0300

    Estruturação da interface Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,3 +1,12 @@
-public class Pessoa {
-    
+public interface Pessoa {
+    String getNome();
+    void setNome(String nome);
+    int getVida();
+    void setVida(int vida);
+    int getForca();
+    void setForca(int forca);
+    int getDefesa();
+    void setDefesa(int defesa);
+    void atacar(Pessoa alvo);
+    void danoRecebido(int dano);
 }
