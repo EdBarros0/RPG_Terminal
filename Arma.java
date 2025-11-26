97b6159950cd6c3c609d4f78cd4022fda39aa77a
eduardoalm <eduardo.hevangelista@gmail.com>
2025-11-26 14:54:26 -0300

    Criação da classe Arma com a função que retorna o dano da arma com a habilidade usada.

--- Arma.java
+++ Arma.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 public class Arma {
-    
+    String nome;
+    int dano;
+    ArrayList<Habilidade> habilidades = new ArrayList<>();
+
+    public Arma(String nome, int dano) {
+        this.nome = nome;
+        this.dano = dano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDano() {
+        return dano;
+    }
+
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+
+    public void adicionarHabilidade(Habilidade habilidade) {
+        habilidades.add(habilidade);
+    }
+    public ArrayList<Habilidade> getHabilidades() {
+        return habilidades;
+    }
+    public int DanoTotal(Habilidade habilidade) {
+        return getDano() * habilidade.poderEfeito();
+    }
 }
